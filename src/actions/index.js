84c2f0d9be3b03a683bf933ef3bd5081174625ff
Henry Buisseret <henrybuisseret@gmail.com>
2020-06-30 14:06:55 +0200

    show ok even when reloaded

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,16 @@
+export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
+
+export function fetchPost(id) {
+  const ROOT_URL = `http://reduxblog.herokuapp.com/api/posts/${id}`;
+  const API_KEY = 'WAGON-BLOG';
+  const promise = fetch(`${ROOT_URL}?key=${API_KEY}`)
+    .then(response => response.json());
+  return {
+    type: FETCH_POST,
+    payload: promise
+  }
+}
 
 export function fetchPosts() {
   const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
@@ -6,7 +18,7 @@ export function fetchPosts() {
   const promise = fetch(`${ROOT_URL}?key=${API_KEY}`)
     .then(response => response.json());
   return {
-    type: FETCH_POST,
+    type: FETCH_POSTS,
     payload: promise
   }
 }
